5990e4218500635bb43d9b0d756e7f482f8e8b8f
nv08-8 <nhuvonguyen2005@gmail.com>
2025-11-14 09:10:51 +0700

    commit

---
+++ BaiTap02/app/src/main/java/vn/hcmute/baitap02_3/LoginActivity.java
@@ -0,0 +1,14 @@
+package vn.hcmute.baitap02_3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ BaiTap02/app/src/main/java/vn/hcmute/baitap02_3/SignupActivity.java
@@ -0,0 +1,14 @@
+package vn.hcmute.baitap02_3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
