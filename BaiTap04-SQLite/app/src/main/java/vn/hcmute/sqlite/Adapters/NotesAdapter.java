8c180d74cfcfaab26ccc18712bb198ba41ee04de
nv08-8 <nhuvonguyen2005@gmail.com>
2025-11-20 10:38:31 +0700

    .

--- BaiTap04-SQLite/app/src/main/java/vn/hcmute/sqlite/Adapters/NotesAdapter.java
+++ BaiTap04-SQLite/app/src/main/java/vn/hcmute/sqlite/Adapters/NotesAdapter.java
@@ -7,20 +7,22 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import vn.hcmute.sqlite.MainActivity;
 import vn.hcmute.sqlite.Models.NotesModel;
 import vn.hcmute.sqlite.R;
 
 public class NotesAdapter extends BaseAdapter {
     //Khai báo biến toàn cục
-    private final Context context;
-    private final int layout;
-    private final ArrayList<NotesModel> notesList;
+    private MainActivity context;
+    private int layout;
+    private ArrayList<NotesModel> notesList;
 
     //Tạo contructor
-    public NotesAdapter(Context context, int layout, ArrayList<NotesModel> notesList) {
+    public NotesAdapter(MainActivity context, int layout, ArrayList<NotesModel> notesList) {
         this.context = context;
         this.layout = layout;
         this.notesList = notesList;
@@ -61,8 +63,26 @@ public class NotesAdapter extends BaseAdapter {
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
-        NotesModel notes = notesList.get(position);
+        final NotesModel notes = notesList.get(position);
         viewHolder.textViewNote.setText(notes.getNameNote());
+
+        //bắt sự kiện nút cập nhật
+        viewHolder.imageViewEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //gọi Dialog trong MainActivity.java
+                context.DialogCapNhatNotes(notes.getNameNote(), notes.getIdNote());
+            }
+        });
+
+        //bắt sự kiện xóa notes
+        viewHolder.imageViewDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context.DialogDelete(notes.getNameNote(), notes.getIdNote());
+            }
+        });
+
         return convertView;
     }
 }
--- BaiTap04-SQLite/app/src/main/java/vn/hcmute/sqlite/MainActivity.java
+++ BaiTap04-SQLite/app/src/main/java/vn/hcmute/sqlite/MainActivity.java
@@ -1,9 +1,20 @@
 package vn.hcmute.sqlite;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -14,49 +25,154 @@ import vn.hcmute.sqlite.Models.NotesModel;
 
 public class MainActivity extends AppCompatActivity {
     //khai báo biến toàn cục
-    DatabaseHandle databaseHandle;
-    ListView listView;
-    ArrayList<NotesModel> arrayList;
-    NotesAdapter notesAdapter;
+     DatabaseHandle databaseHandle;
+     ListView listView;
+     ArrayList<NotesModel> arrayList;
+     NotesAdapter notesAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listView1);
-        arrayList = new ArrayList<>();
-        notesAdapter = new NotesAdapter(this, R.layout.item_notes, arrayList);
-        listView.setAdapter(notesAdapter);
+         //ánh xạ listview và gọi adapter
+         listView = findViewById(R.id.listView1);
+         arrayList = new ArrayList<>();
+         notesAdapter = new NotesAdapter(this, R.layout.item_notes, arrayList);
+         listView.setAdapter(notesAdapter);
 
-        //khởi tạo database
-        databaseHandle = new DatabaseHandle(this);
+         //gọi hàm databaseSQLite
+         InitDatabaseSQLite();
+         createDatabaseSQLite();
+         databaseSQLite();
+     }
 
-        // Load notes to check if the database is empty
-        loadNotes();
+     private void createDatabaseSQLite() {
+         //thêm dữ liệu vào bảng
+         databaseHandle.queryData("INSERT INTO " + DatabaseHandle.TABLE_NAME + " VALUES (null, 'Ví dụ SQLite 1')");
+         databaseHandle.queryData("INSERT INTO " + DatabaseHandle.TABLE_NAME + " VALUES (null, 'Ví dụ SQLite 2')");
+     }
 
-        // If the list is empty, it's likely the first run, so add sample data
-        if (arrayList.isEmpty()) {
-            createSampleData();
-            // Reload notes after adding data
-            loadNotes();
-        }
-    }
-
-    private void createSampleData() {
-        databaseHandle.queryData("INSERT INTO " + DatabaseHandle.TABLE_NAME + " (NameNote) VALUES ('Ví dụ 1')");
-        databaseHandle.queryData("INSERT INTO " + DatabaseHandle.TABLE_NAME + " (NameNote) VALUES ('Ví dụ 2')");
-    }
-
-    private void loadNotes() {
+     private void InitDatabaseSQLite() {
+         //khởi tạo database
+         databaseHandle = new DatabaseHandle(this);
+     }
+    public void databaseSQLite() {
+         //Lấy dữ liệu
         Cursor cursor = databaseHandle.getData("SELECT * FROM " + DatabaseHandle.TABLE_NAME);
         arrayList.clear();
         while (cursor.moveToNext()) {
-            int id = cursor.getInt(0);
             String name = cursor.getString(1);
+            int id = cursor.getInt(0);
             arrayList.add(new NotesModel(id, name));
         }
         notesAdapter.notifyDataSetChanged();
-        cursor.close();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         getMenuInflater().inflate(R.menu.menu, menu);
+         return super.onCreateOptionsMenu(menu);
+    }
+
+    //bắt sự kiện cho menu
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menuAddNotes) {
+            DialogThem();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void DialogThem() {
+        //thêm dialog
+        Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_add_notes);
+        //ánh xạ trong dialog
+        EditText editText = dialog.findViewById(R.id.editTextName);
+        Button buttonAdd = dialog.findViewById(R.id.buttonEdit);
+        Button buttonHuy = dialog.findViewById(R.id.buttonHuy);
+        //bắt sự kiện cho nút thêm và hủy
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                if (name.equals("")) {
+                    Toast.makeText(MainActivity.this, "Vui lòng nhập tên", Toast.LENGTH_SHORT).show();
+                } else {
+                    databaseHandle.queryData("INSERT INTO " + DatabaseHandle.TABLE_NAME + " VALUES (null, '" + name + "')");
+                    Toast.makeText(MainActivity.this, "Đã thêm notes", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                    databaseSQLite(); //gọi hàm load lại dữ liệu
+                }
+            }
+
+        });
+        buttonHuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+
+    }
+
+    //hàm dialog cập nhật
+    public void DialogCapNhatNotes(String tenCV, final int id) {
+        Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_update_notes);
+
+        EditText editText = dialog.findViewById(R.id.editTextNameUpdate);
+        Button buttonUpdate = dialog.findViewById(R.id.buttonUpdate);
+        Button buttonCancel = dialog.findViewById(R.id.buttonCancel);
+
+        editText.setText(tenCV);
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tenMoi = editText.getText().toString().trim();
+                if (tenMoi.equals("")) {
+                    Toast.makeText(MainActivity.this, "Vui lòng nhập tên công việc", Toast.LENGTH_SHORT).show();
+                } else {
+                    databaseHandle.queryData("UPDATE " + DatabaseHandle.TABLE_NAME + " SET " + DatabaseHandle.COL_NAME + " = '" + tenMoi + "' WHERE " + DatabaseHandle.COL_ID + " = " + id);
+                    Toast.makeText(MainActivity.this, "Đã cập nhật", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                    databaseSQLite();
+                }
+            }
+        });
+
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    //hàm dialog xóa
+    public void DialogDelete(String name, final int id){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Bạn có muốn xóa Notes " + name + " này không ?");
+        builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                databaseHandle.queryData("DELETE FROM " + DatabaseHandle.TABLE_NAME + " WHERE " + DatabaseHandle.COL_ID + " = " + id);
+                Toast.makeText(MainActivity.this, "Đã xóa Notes " + name+ " thành công", Toast.LENGTH_SHORT).show();
+                databaseSQLite();//gọi hàm load lại dữ liệu
+            }
+        });
+        builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.show();
     }
 }
