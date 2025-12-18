3eef64b14e44a64018619d2bdea267e1ac547f68
nv08-8 <nhuvonguyen2005@gmail.com>
2025-12-18 10:29:21 +0700

    Revert "."
    This reverts commit a5944f1e2049737bec225b8661ffa4b1ddb92cf3.

--- BaiTap09/app/src/main/java/vn/hcmute/baitap09/MainActivity.java
+++ BaiTap09/app/src/main/java/vn/hcmute/baitap09/MainActivity.java
@@ -1,8 +1,6 @@
 package vn.hcmute.baitap09;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,17 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-/**
- * MainActivity - Entry point của ứng dụng
- *
- * Cung cấp 2 tùy chọn:
- * 1. Mở chat với role Customer
- * 2. Mở chat với role Manager
- */
 public class MainActivity extends AppCompatActivity {
-
-    private Button btnCustomerChat;
-    private Button btnManagerChat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,28 +19,6 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        initViews();
-        setupListeners();
-    }
-
-    private void initViews() {
-        btnCustomerChat = findViewById(R.id.btnCustomerChat);
-        btnManagerChat = findViewById(R.id.btnManagerChat);
-    }
-
-    private void setupListeners() {
-        // Open customer chat
-        btnCustomerChat.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, ChatActivity.class);
-            startActivity(intent);
-        });
-
-        // Open manager chat
-        btnManagerChat.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, ManagerChatActivity.class);
-            startActivity(intent);
         });
     }
 }
